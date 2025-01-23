d7787cc49bfef16362b711adbb4ec0901ca93bc9
Alex <aleksandrdorofeev04@gmail.com>
2025-01-23 12:37:08 +0500

    Update spearman.gd

--- characters/spearman/spearman.gd
+++ characters/spearman/spearman.gd
@@ -32,8 +32,8 @@ func _process(delta: float):
 	var stick_y = Input.get_action_strength("look_down") - Input.get_action_strength("look_up")
 	
 	if abs(stick_x) > 0 or abs(stick_y) > 0:
-		input_rotation_x = -stick_y * JOYSTICK_SENSITIVITY
-		input_rotation_y = -stick_x * JOYSTICK_SENSITIVITY
+		input_rotation_x = -stick_x * JOYSTICK_SENSITIVITY
+		input_rotation_y = -stick_y * JOYSTICK_SENSITIVITY
 	
 	current_rotation_x = lerp(current_rotation_x, input_rotation_x, SMOOTHING * delta)
 	current_rotation_y = lerp(current_rotation_y, input_rotation_y, SMOOTHING * delta)
@@ -41,9 +41,9 @@ func _process(delta: float):
 	input_rotation_x = 0.0
 	input_rotation_y = 0.0
 	
-	camera.rotation.x += current_rotation_x
+	camera.rotation.x += current_rotation_y
 	camera.rotation.x = clamp(camera.rotation.x, -MAX_VERTICAL_ANGLE, MAX_VERTICAL_ANGLE)
-	rotation.y += current_rotation_y
+	rotation.y += current_rotation_x
 
 
 func _physics_process(delta):
@@ -62,8 +62,8 @@ func _physics_process(delta):
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		input_rotation_x = -deg_to_rad(event.relative.y) * MOUSE_SENSITIVITY
-		input_rotation_y = -deg_to_rad(event.relative.x) * MOUSE_SENSITIVITY
+		input_rotation_x = -deg_to_rad(event.relative.x) * MOUSE_SENSITIVITY
+		input_rotation_y = -deg_to_rad(event.relative.y) * MOUSE_SENSITIVITY
 	
 	if event.is_action_pressed("attack"):
 		spear.call("attack")
