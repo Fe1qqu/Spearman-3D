30027934f0c6c9a4afc4649b9241ade5c554e550
Alex <aleksandrdorofeev04@gmail.com>
2025-01-23 12:36:59 +0500

    update menu

--- other/main_menu.gd
+++ other/main_menu.gd
@@ -4,13 +4,13 @@ var hello_screen_active: bool = true
 
 func _ready() -> void:
 	$TextureRect.texture = load("res://textures/hello_screen.png")
-	$MarginContainer.hide()
+	$CenterContainer.hide()
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and hello_screen_active:
+	if (event is InputEventKey or event is InputEventMouseButton) and event.pressed and hello_screen_active:
 		$TextureRect.texture = load("res://textures/main_menu_screen.png")
-		$MarginContainer.show()
+		$CenterContainer.show()
 
 
 func _on_new_game_button_pressed() -> void:
