a7eaab1491a548562b686e75eb6e7d9c71e8bc94
Alex <aleksandrdorofeev04@gmail.com>
2025-11-26 00:39:22 +0500

    Emit hit_target signal when damage is dealt
    Added the hit_target signal to HitBox and emit it from HurtBox when damage is dealt. This allows external systems to react when a hitbox successfully hits a target.

--- other/hitbox.gd
+++ other/hitbox.gd
@@ -2,3 +2,10 @@ class_name HitBox
 extends Area3D
 
 @export var damage: int = 1
+
+signal hit_target(target: Node)
+
+func _ready() -> void:
+	# Prevent "UNUSED_SIGNAL" warning (signal is emitted externally from HurtBox)
+	if false:
+		emit_signal("hit_target", null)
