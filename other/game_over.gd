5b19dbb06c6794a6a3c0032392662cdd116f506c
Alex <aleksandrdorofeev04@gmail.com>
2025-11-26 00:40:21 +0500

    Allow quitting during game over animation
    Removed the check for AnimationPlayer playing state, enabling the user to quit to the main menu at any time when the 'quit' action is pressed, even if the animation is still playing.

--- other/game_over.gd
+++ other/game_over.gd
@@ -4,6 +4,6 @@ func _ready() -> void:
 	$AnimationPlayer.play("animation")
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and not $AnimationPlayer.is_playing() and event.is_action_pressed("quit"):
+	if event is InputEventKey and event.is_action_pressed("quit"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		get_tree().change_scene_to_file("res://other/main_menu.tscn")
